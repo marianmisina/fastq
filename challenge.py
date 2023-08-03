e76089934aafd4d239b66992fe553a96a9f6cdcc
Marian <marian@Marians-MacBook-Air.local>
2023-08-03 22:17:29 +0200

    Added docstrings. Script now takes parameters from command line. Lutpu file are created with timestamos in their names.

--- challenge.py
+++ challenge.py
@@ -1,8 +1,19 @@
-SEQUENCE_SIZE = 80
+import sys
+import time
+
 DICT_ACGT = {0:'A', 1:'C', 2:'G', 3:'T'}
-FILENAME = "input"
+FASTQ_FORMAT = '@READ_{index}\n{dna_sequence}\n+READ_{index}\n{quality_score}\n'
 
 def file_reader(filename, sequence_size):
+    """Python generator that yields chunks of bytes from the input file.
+
+    Args:
+        filename (str): name/path of the input file
+        sequence_size (int): size of the byte chunks
+
+    Yields:
+        bytes: string of bytes
+    """
     with open(filename, "rb") as input_file:
         while True:
             chunk_of_bytes = input_file.read(sequence_size)
@@ -13,6 +24,17 @@ def file_reader(filename, sequence_size):
                 break
 
 def process_piece(chunk_of_bytes):
+    """Parse bytes into dna sequence and quality score strings.
+    The first two (most significant) bits of each byte encode the DNA letter
+    The last six (least significant) bits of each byte encode the quality score.
+
+    Args:
+        chunk_of_bytes (bytes): bytes to be decoded
+
+    Returns:
+        str: decoded DNA sequence
+        str: quality score
+    """
     sequence = ""
     quality = ""
     for _ in chunk_of_bytes:
@@ -20,10 +42,26 @@ def process_piece(chunk_of_bytes):
         sequence += str(DICT_ACGT[_ >> 6])
     return sequence, quality
 
-index = 0
-for piece in file_reader(FILENAME, SEQUENCE_SIZE):
-    index += 1
-    dna_sequence, quality_score = process_piece(piece)
-    with open("output.txt", "a", encoding="utf-8") as output_file:
-        output_file.write(f'@READ_{index}\n{dna_sequence}\n+READ_{index}\n{quality_score}\n')
-        output_file.close()
+def create_fastq_file(filename,sequence_size):
+    """Read input file and decode it.
+    Create new file and save the date ib new file in FASTQ format.
+
+    Args:
+        filename (str): _description_
+        sequence_size (int): _description_
+    """
+    index = 0
+    timestamo = time.time()
+    for piece in file_reader(filename, sequence_size):
+        index += 1
+        dna_sequence, quality_score = process_piece(piece)
+        with open(f'output_{timestamo}.txt', "a", encoding="utf-8") as output_file:
+            output_file.write(FASTQ_FORMAT.format(index=index,
+                                                  dna_sequence=dna_sequence,
+                                                  quality_score=quality_score))
+            output_file.close()
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    sequence_size  = int(sys.argv[2])
+    create_fastq_file(filename, sequence_size)
